1b0fe46473bbdaa71e3d33df566b6007b980a276
Lovelex <lukas489@gmail.com>
2018-09-06 13:23:01 -0300

    mod math.js, mod numberCuidades.js, add pasta string, add string.js

--- fundamentos/number/numberCuidados.js
+++ fundamentos/number/numberCuidados.js
@@ -1,4 +1,5 @@
 console.log(10 / 0) // retorna: Infinity
-console.log("10" / 5) // retorna 2   
-console.log( "palavra" * 2 ) // retorna NaN
-console.log( 0.1 + 0.7 ) // retorna 0.7999999999999999
+console.log("10" / 5) // retorna: 2
+console.log("10" + 5) // retorna: 105   
+console.log( "palavra" * 2 ) // retorna: NaN (Not a Number)
+console.log( 0.1 + 0.7 ) // retorna: 0.7999999999999999
---
+++ fundamentos/string/string.js
@@ -0,0 +1,22 @@
+const palavra = "L3TRA"
+
+console.log( palavra.charAt(4) ) // retorna: A
+console.log( palavra.charAt(5) ) // retorna: 
+console.log( palavra.charCodeAt(4) ) // retorna: 65
+console.log( palavra.indexOf('3') ) // retorna: 1
+console.log( palavra.substring(1) ) // retorna: 3TRA
+console.log( palavra.substring( 1, 4 ) ) // retorna: 3TR
+console.log( "L3".concat("TRA") ) // retorna: L3TRA
+console.log( "L3" + "TRA" ) // retorna: L3TRA
+console.log( palavra.replace( 3, 'E' ) ) // retorna: LETRA
+console.log( 'L,E,T,R,A'.split(',')) // retorna [L,E,T,R,A]
+
+/*
+ * charAt() retorna a letra contida na palavra de acordo com a posição passada por parâmetro (se a posição não existir, retorna vazio)
+ * charCodeAt() retorna o valor da letra em Unicode de acordo com a posição da letra na palavra passada por parâmetro https://unicode-table.com/pt/#control-character
+ * indexOf() retorna a posição da letra na palavra de acordo com a letra passada por parâmetro
+ * substring() retorna uma quantidade de letras especifica, se 1 valor for passado, retorna da posição ate o fina. Se duas, retorna do parâmetro 1 ate o parâmetro 2
+ * concat() concatena uma string com outra
+ * replace() modifica um valor de uma letra, primeiro parâmetro espera pelo alvo, o segundo prâmetro espera pela letra a ser substituída
+ * split() transforma uma string em um array a partir de um separador determindao passado por parâmetro
+*/ 
