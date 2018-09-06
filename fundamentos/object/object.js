d2158474ce612ab214dd7acfd40726049443bcb4
Lovelex <lukas489@gmail.com>
2018-09-06 16:44:45 -0300

    add object.js

---
+++ fundamentos/object/object.js
@@ -0,0 +1,9 @@
+const obj1 = {produto : 'Nintendo Switch', preço: 1300  }
+
+console.log(obj1) // retorna: { produto: 'Nintendo Switch', 'preço': 1300 }
+
+const obj2 = {}
+obj2.produto = 'Nintendo Switch'
+obj2.preço = 1300
+
+console.log(obj2) // retorna: { produto: 'Nintendo Switch', 'preço': 1300 }
