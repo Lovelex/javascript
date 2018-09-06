57c105ed3df0253659763923f25a5228a6afe232
Lovelex <lukas489@gmail.com>
2018-09-06 09:28:13 -0300

    add pasta basico, add VarLetConst.js

---
+++ fundamentos/basico/VarLetConst.js
@@ -0,0 +1,11 @@
+var a = "Declarado com VAR"
+let b = "Declarado com LET" 
+const c = "Declarado com CONST"
+
+console.log(a + "\n" + b + "\n" + c)
+
+/* 
+ * Variáveis declaradas com Var podem ser re-declaradas.
+ * Variáveis declaradas com Let não podem ser re-declaradas.
+ * Constantes são declaradas com Const e não podem ser variadas.
+*/ 
