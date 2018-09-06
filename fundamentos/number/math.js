3564852a727db642dee2be9ae20ade3f7bb1c187
Lovelex <lukas489@gmail.com>
2018-09-06 11:08:02 -0300

    math.js

---
+++ fundamentos/number/math.js
@@ -0,0 +1,12 @@
+const raio = 5.6
+const area = Math.PI * Math.pow( raio, 2 )
+
+console.log(area) // retorna: 98.5203456165759
+console.log( Math.PI ) // retorna 3.141592653589793
+
+/*
+ * Math.pow() cria uma conta matemática de potência.
+ * você deve passar dois parâmetros, o primeiro 
+ * sendo o número que que fazer a operação e o
+ * segundo por quanto você deseja elevar.
+*/ 
