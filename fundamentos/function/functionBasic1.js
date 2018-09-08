44c0013459a7c8ec7ae4ca3dc4372d8334cd8fe5
Lovelex <lukas489@gmail.com>
2018-09-08 18:23:21 -0300

    add functionBasic.js

---
+++ fundamentos/function/functionBasic1.js
@@ -0,0 +1,21 @@
+// Função sem retorno
+
+function imprimirSoma( a, b ){
+    console.log( a + b )
+} // mostra no console o resultado de ( a + b )
+
+imprimirSoma( 5, 5 ) // mostra no console: 10
+imprimirSoma( 5 ) // mostra no console: NaN ( 5 + Undefinied = NaN )
+imprimirSoma( 5, 4, 3, 2, 1 ) // mostra no console: 9 ( 3, 2, 1 são ignorados )
+imprimirSoma() // mostra no console: NaN ( Undefinied + Undefinied = NaN )
+
+// Função com retorno
+
+function soma( a, b = 5 ){ // b = 5 significa que o valor padrão será 5, então se b não for passado, considerará 5
+    return a + b
+}
+
+console.log( soma( 5 ) ) // retorna: 10
+console.log( soma( 5, 5 ) ) // retorna: 10
+console.log( soma(2, 2) ) // retorna: 4
+console.log( soma( ) ) // retorna: NaN ( undefinied + 5 = NaN )
