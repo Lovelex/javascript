c93be600b7cb02044b7cd3275ff6c04485f668d5
Lovelex <lukas489@gmail.com>
2018-09-22 05:52:57 -0300

    add argumentosPadroes.js

---
+++ funcoes/argumentosPadroes.js
@@ -0,0 +1,10 @@
+function soma( a = 1, b = 1, c = 1 ){
+
+    return a + b + c
+
+}
+
+console.log( soma() ) // retorna: 3
+console.log( soma( 5 ) ) // retorna: 7
+console.log( soma( 5, 5 ) ) // retorna: 11
+console.log( soma( 5, 5, 5 ) ) // retorna: 15
