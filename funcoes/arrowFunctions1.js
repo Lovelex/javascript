793cc4843a868c44d4d2df1e3aec5888bf66f4db
Lovelex <lukas489@gmail.com>
2018-09-23 04:57:32 -0300

    mod arrowFunction1.js

--- funcoes/arrowFunctions1.js
+++ funcoes/arrowFunctions1.js
@@ -1,13 +1,18 @@
 let dobro = ( a ) => { return 2 * a }
-
 console.log( dobro( 2 ) ) // retorna: 4
 
 
 dobro = ( a ) => 2 * a
-
 console.log( dobro( 2 ) ) // retorna: 4
 
 
 dobro = ( a ) => { 2 * a } 
-
 console.log( dobro( 2 ) ) // retorna: undefinied ( quando possuir bloco de código {}, de ter o return incluso senão retornará undefined )
+
+/* ***************************************** */
+
+let falar = () => 'Hello, Wolrd!'
+console.log( falar() ) // retorna: Hello, Wolrd
+
+falar = _ => 'Hello, Wolrd!'
+console.log( falar() )
