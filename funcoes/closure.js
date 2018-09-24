7c6eff0df64dafaa269a2d137de7b9c406697434
Lovelex <lukas489@gmail.com>
2018-09-24 08:51:55 -0300

    add closure.js

---
+++ funcoes/closure.js
@@ -0,0 +1,23 @@
+const value = 'Global'
+
+function showValue(){
+
+    const value = 'Local'
+
+    function returnValue(){
+
+        return value
+
+    }
+    return returnValue
+    
+}
+
+const valueReturned = showValue()
+
+console.log( valueReturned() ) // retorna: Local
+
+/*
+ * Closure é o escopo criado quando uma função é declarada
+ * Esse escopo permite a função acessar e manipular variáveis esternas à função
+*/ 
---
+++ test.js
@@ -0,0 +1,11 @@
+function accum(s) {
+	
+    let letters = s.split('')
+
+    console.log( letters.length )
+
+    for( let i = 0; letters.length > i; i++ )
+
+  }
+
+  accum('trab')
