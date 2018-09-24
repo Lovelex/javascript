3481aa1e7a3772af5a86010212c6f1bd262b76b4
Lovelex <lukas489@gmail.com>
2018-09-24 10:15:56 -0300

    mod factoryFunction.js

--- funcoes/factoryFunction1.js
+++ funcoes/factoryFunction1.js
@@ -8,3 +8,7 @@ function criarPessoa(){
 console.log( criarPessoa() ) // retorna: { nome: 'Lucas', sobrenome: 'Humberto' }
 console.log( criarPessoa().nome ) // retorna: Lucas
 console.log( criarPessoa().sobrenome ) // retorna: Humberto
+
+/*
+ * Função Factory é uma função que retorna um objeto
+*/ 
