7243dd8aa140df77b82845d77930ad94f09e9ee5
Lovelex <lukas489@gmail.com>
2018-09-27 15:23:47 -0300

    add funcoesObjetos.js

---
+++ objetos/funcoesObjetos.js
@@ -0,0 +1,29 @@
+const pessoa = { nome: 'Lucas', idade: 24 }
+
+console.log( Object.keys( pessoa ) ) // retorna: [ 'nome', 'idade' ]
+console.log( Object.values( pessoa ) ) // retorna: [ 'Lucas', 24 ]
+console.log( Object.entries( pessoa ) ) // retorna: [ [ 'nome', 'Lucas' ], [ 'idade', 24 ] ]
+
+Object.defineProperty( pessoa, 'dataNascimento', {
+    enumerable: true, // vai aparecer na lista?
+    writable: false, // pode ser sobrescrito?
+    value: '12/11/1993' // valor da propriedade
+} )
+
+console.log( pessoa ) // retorna: { nome: 'Lucas', idade: 24, dataNascimento: '12/11/1993' }
+
+Object.defineProperty( pessoa, 'segredo', {
+    enumerable: false, // vai aparecer na lista?
+    writable: false, // pode ser sobrescrito?
+    value: 'não aparecerá' // valor da propriedade
+} )
+
+console.log( pessoa ) // retorna: { nome: 'Lucas', idade: 24, dataNascimento: '12/11/1993' }
+
+const destino = { a: 1 }
+const o1 = { b: 2 }
+const o2 = { c: 3, a: 4 }
+
+Object.assign( destino, o1, o2 )
+
+console.log( destino )
