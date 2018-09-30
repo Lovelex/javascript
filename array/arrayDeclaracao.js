9e450866d8656730f414862c84bd2f10e991e868
Lovelex <lukas489@gmail.com>
2018-09-30 14:05:17 -0300

    mod arrayDeclaracao.js

--- array/arrayDeclaracao.js
+++ array/arrayDeclaracao.js
@@ -6,3 +6,6 @@ pessoas[0] = 'Bia'
 pessoas[1] = 'Anderson'
 pessoas.push( 'Carmem' )
 console.log( pessoas ) // retorna: [ 'Bia', 'Anderson', 'Carmem' ] 
+
+delete pessoas[0]
+console.log( pessoas ) // retorna: [ <1 empty item>, 'Anderson', 'Carmem' ]
