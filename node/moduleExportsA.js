1f8ad56a586064e0258ce2f6fae10212fed897b0
Lovelex <lukas489@gmail.com>
2018-10-05 14:50:16 -0300

    add moduleExportsA.js

---
+++ node/moduleExportsA.js
@@ -0,0 +1,9 @@
+this.sayHello = 'Hello, Wolrd!'
+exports.welcome = 'Welcome!'
+module.exports.bye = 'Good bye!'
+
+/*
+ * Os exemplos acima são formas de exportar um módulo ou deixa-los visíveis para outros arquivos
+ * importalos.
+ * Esses exemplos são as formas menos utilizadas para exportar.
+*/
