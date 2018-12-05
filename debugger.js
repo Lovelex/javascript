4db334254bf05aa9ded30e3bcfc77c6595c5379d
Lovelex <lukas489@gmail.com>
2018-12-05 13:53:42 -0200

    css

--- debugger.js
+++ debugger.js
@@ -1,1 +1,9 @@
-console.log( 'O' == 'O' )
+function Carro( marca, modelo, ano ){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano
+}
+
+const meuCarro = new Carro( "Eagle", "Talon TSi", 1993 )
+
+console.log( meuCarro )
