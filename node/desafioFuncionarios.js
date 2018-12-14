a2736fcd4a2e408affdc0eed9e095e93038d05cf
Lovelex <lukas489@gmail.com>
2018-12-14 21:43:30 -0200

    dom

--- node/desafioFuncionarios.js
+++ node/desafioFuncionarios.js
@@ -2,19 +2,17 @@ const axios = require('axios')
 
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 
-const getUrlResponse =  response => {
-    const funcionarios = response.data
-    
-    const orderSalarioByCresc = ( a, b ) => a.salario - b.salario
-    const getChinese = ( f ) => f.pais === 'China'
-    const getFemale = ( f ) => f.genero === 'F'
+const getUrlResponse = response => {
+  const funcionarios = response.data
+  const orderSalarioByCresc = (a, b) => a.salario - b.salario
+  const getChinese = (f) => f.pais === 'China'
+  const getFemale = (f) => f.genero === 'F'
 
-    const funcionariosFiltrados = funcionarios.filter( getChinese )
-    .filter( getFemale )
-    .sort( orderSalarioByCresc )
+  const funcionariosFiltrados = funcionarios.filter(getChinese)
+    .filter(getFemale)
+    .sort(orderSalarioByCresc)
 
-    console.log( funcionariosFiltrados[0] )
-
+  console.log(funcionariosFiltrados[0])
 }
 
-axios.default(url).then( getUrlResponse )
+axios.default(url).then(getUrlResponse)
