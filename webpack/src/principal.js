d9bd313856a43acc6fe4b3b13f4dfc6ed37ccc39
Lovelex <lukas489@gmail.com>
2018-12-24 21:23:55 -0200

    packagejs

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,4 +1,4 @@
 import Pessoa from './pessoa'
-import './assets/scss' // procura dentro de assets o arquivo index.js
+import './assets' // procura dentro de assets o arquivo index.js
 const atendente = new Pessoa;
 console.log(atendente.cumprimentar());
