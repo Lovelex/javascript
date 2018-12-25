79982f6398fb1151191caaeb2a2c4aa40d28cb90
Lovelex <lukas489@gmail.com>
2018-12-25 13:06:10 -0200

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,29 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const uglifyjsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-  mode: 'production', // production para setar modo de produção. development para modo desenvolvimento. por padrão é production
+  // mode: 'production', // production para setar modo de produção. development para modo desenvolvimento. por padrão é production
+  mode: modoDev ? 'development' : 'production',
   entry: './src/principal.js', // arquivo a ser interpretado
   output: {
     filename: 'principal.js', // Nome do arquivo gerado
     path: __dirname + '/public' // path é á pasta gerada, __dirname aponta para a pasta raiz. Por padrão é pasta dist
+  },
+  devServer: {
+    contentBase: './public',
+    port: 9000
+  },
+  optimization: {
+    minimizer: [
+      new uglifyjsPlugin({ // minifica o arquivo js
+        cache: true,
+        parallel: true // otimiza o processo de minificação 
+      }),
+      new OptimizeCSSAssetsPlugin({}) // minifica o arquivo css
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -22,6 +39,9 @@ module.exports = {
         'css-loader', // interpreta @import, url(), etc
         'sass-loader'
       ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: ['file-loader'] // carrega arquivos de imagem
     }]
   }
 }
