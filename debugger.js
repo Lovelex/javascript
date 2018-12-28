a0be16f5e527b9c5c3f69aac79ddbaf45128ece7
Lovelex <lukas489@gmail.com>
2018-12-28 17:30:02 -0200

    jQuery

--- debugger.js
+++ debugger.js
@@ -1,3 +1,31 @@
-const nulo = null
+function Calculadora() {
+  let resultado = 0;
+  this.somar = (...nums) => {
+    resultado += eval(nums.join('+'));
+    return this;
+  }
+  this.subtrair = (...nums) => {
+    resultado -= eval(nums.join('-'));
+    return this;
+  }
+  this.multplicar = (...nums) => {
+    resultado *= eval(nums.join('*'));
+    return this;
+  }
+  this.dividir = (...nums) => {
+    resultado /= eval(nums.join('/'));
+    return this;
+  }
+  this.zerar = () => {
+    resultado = 0;
+    return this;
+  }
+  this.showResults = () => {
+    console.log(resultado)
+    return this;
+  }
+}
 
-if(nulo) console.log('Sou nulo')
+const calculadora = new Calculadora()
+
+calculadora.somar(11, 80).showResults()
