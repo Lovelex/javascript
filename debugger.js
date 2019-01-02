007800a59ac6950e6770cae4c1dc586d1fd3ec09
Lovelex <lukas489@gmail.com>
2019-01-02 10:19:05 -0200

    jQuery

--- debugger.js
+++ debugger.js
@@ -1,31 +1,11 @@
-function Calculadora() {
-  let resultado = 0;
-  this.somar = (...nums) => {
-    resultado += eval(nums.join('+'));
-    return this;
-  }
-  this.subtrair = (...nums) => {
-    resultado -= eval(nums.join('-'));
-    return this;
-  }
-  this.multplicar = (...nums) => {
-    resultado *= eval(nums.join('*'));
-    return this;
-  }
-  this.dividir = (...nums) => {
-    resultado /= eval(nums.join('/'));
-    return this;
-  }
-  this.zerar = () => {
-    resultado = 0;
-    return this;
-  }
-  this.showResults = () => {
-    console.log(resultado)
-    return this;
-  }
+const seq = [1, 2, 1]
+
+function almostIncreasingSequence(sequence) {
+  const arr = sequence.sort((a, b) =>  a - b)
+  console.log(arr)
+  arr.every((value, i) => {
+    console.log( value ) 
+})
 }
 
-const calculadora = new Calculadora()
-
-calculadora.somar(11, 80).showResults()
+console.log( almostIncreasingSequence(seq) )
